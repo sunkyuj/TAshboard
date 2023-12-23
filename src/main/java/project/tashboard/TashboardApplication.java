6af0df7c9c32a5906e5e3f044ed488d102f1ab85
sunkyuj <sunkyuj12@gmail.com>
2023-12-23 21:23:47 +0900

    init

---
+++ src/main/java/project/tashboard/TashboardApplication.java
@@ -0,0 +1,13 @@
+package project.tashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/tashboard/TashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package project.tashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
